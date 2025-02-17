e99c5443438baff02fb90a46dbf5b3a58a49897b
priya-1401 <108124861+priya-1401@users.noreply.github.com>
2025-02-17 12:17:26 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -38,16 +38,16 @@ export default function TextForm(props) {
     const [text, setText] = useState('');
   return (
     <>
-    <div className="container" style={{backgroundcolor:props.mode==='light'?'dark':'light'}}>
+    <div className="container" style={{backgroundColor:back_color(),color:props.mode==='light'?'black':'#fff'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: c(), color:'black'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className="btn btn-primary" onClick={handleLoClick}>Convert to Uppercase</button>
-            <button type="submit" onClick={speak} className="btn btn-primary mx-2 my-2">Speak</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick} disabled={text.length===0}>Convert to Uppercase</button>
+            <button className="btn btn-primary max-1 my-1" onClick={handleLoClick} disabled={text.length===0}>Convert to Lowercase</button>
+            <button type="submit" onClick={speak} disabled={text.length===0} className="btn btn-primary mx-2 my-2">Speak</button>
     </div>
-     <div className="container my-3" style={{backgroundColor:props.mode==='light'?'dark':'light'}}>
+     <div className="container my-3" style={{backgroundColor:back_color(),color:props.mode==='light'?'black':'white'}}>
         <h1>The Text Summary</h1>
         <p>The Number of words is {text.split(" ").filter((element)=>{return element.length!==0}).length} and Number of letters are {text.length}</p>
         <p>{0.008 * text.length} minutes required to read</p>
